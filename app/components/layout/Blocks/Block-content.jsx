07fbae6bbce9b2a4bed92ab9ecdf0e3e4e392c96
Felipe-Harrison <harrisufelipe@gmail.com>
2023-12-26 18:54:22 -0300

    fix favicon

--- app/components/layout/Blocks/Block-content.jsx
+++ app/components/layout/Blocks/Block-content.jsx
@@ -7,7 +7,7 @@ const BlockContent = ({id="#id",title="Titulo Aqui",children}) => {
     return(
         <section id={id} className="
             flex flex-col gap-5 place-center text-center 
-            py-5 md:scroll-mt
+            py-5 scroll-mt
         ">
             <h2 className="text-2xl font-bold">{title}</h2>
             {children}
